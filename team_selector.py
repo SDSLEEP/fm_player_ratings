5158420b63991994d16f72b698861bcfa69f6f20
Egraam <egraam2020@gmail.com>
2021-05-24 09:23:25 +0200

    variable weigths -> weights

--- team_selector.py
+++ team_selector.py
@@ -33,57 +33,57 @@ for i in range(len(df)):
     amc_match = re.search(r"\bAM\s?\(.{0,3}C.*\)", pos)
     st_match = re.search(r"\bST.*", pos)
     if pos == 'GK':
-        weigths = [0.12, 0.12, 0.10, 0.09]
+        weights = [0.12, 0.12, 0.10, 0.09]
         attributes = [player['Han'], player['Ref'], player['Dec'], player['Agi']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         gk[player['Name']] = rating
     if dl_match or dr_match:
-        weigths = [0.15, 0.14, 0.13, 0.11, 0.07, 0.07, 0.07]
+        weights = [0.15, 0.14, 0.13, 0.11, 0.07, 0.07, 0.07]
         attributes = [player['Acc'], player['Pos'], player['Dec'], player['Pac'],
                       player['Tck'], player['Cnt'], player['Agi']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         if dl_match:
             dl[player['Name']] = rating
         if dr_match:
             dr[player['Name']] = rating
     if dc_match:
-        weigths = [0.13, 0.10, 0.10, 0.09, 0.08, 0.08, 0.08]
+        weights = [0.13, 0.10, 0.10, 0.09, 0.08, 0.08, 0.08]
         attributes = [player['Dec'], player['Pos'], player['Mar'], player['Acc'],
                       player['Jum'], player['Str'], player['Pac']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         dc[player['Name']] = rating
     if dm_match:
-        weigths = [0.12, 0.11, 0.10, 0.08, 0.07, 0.07]
+        weights = [0.12, 0.11, 0.10, 0.08, 0.07, 0.07]
         attributes = [player['Acc'], player['Dec'], player['Tck'], player['Pac'],
                       player['Str'], player['Agi']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         dm[player['Name']] = rating
     if mc_match:
-        weigths = [0.12, 0.11, 0.10, 0.10, 0.07, 0.07]
+        weights = [0.12, 0.11, 0.10, 0.10, 0.07, 0.07]
         attributes = [player['Acc'], player['Vis'], player['Pas'], player['Pac'],
                       player['Dec'], player['Agi']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         mc[player['Name']] = rating
     if ml_match or mr_match:
-        weigths = [0.26, 0.20, 0.07]
+        weights = [0.26, 0.20, 0.07]
         attributes = [player['Acc'], player['Pac'], player['Agi']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         if ml_match:
             ml[player['Name']] = rating
         if mr_match:
             mr[player['Name']] = rating
     if aml_match or amr_match:
-        weigths = [0.28, 0.28, 0.07]
+        weights = [0.28, 0.28, 0.07]
         attributes = [player['Acc'], player['Pac'], player['Dri']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         if aml_match:
             aml[player['Name']] = rating
         if amr_match:
             amr[player['Name']] = rating
     if amc_match:
-        weigths = [0.23, 0.13, 0.09]
+        weights = [0.23, 0.13, 0.09]
         attributes = [player['Acc'], player['Pac'], player['Vis']]
-        rating = round(np.dot(weigths, attributes) * 1 / sum(weigths), 2)
+        rating = round(np.dot(weights, attributes) * 1 / sum(weights), 2)
         amc[player['Name']] = rating
     if st_match:
         weigths_fs = [0.24, 0.17, 0.08]
